39fd29a3c1409caead0def52c8a0b0a67e2ce68b
volcain-io <github@volcain.io>
2017-11-16 17:34:09 +0100

    Add comments to code & some minor bug fixes.

--- js/app.js
+++ js/app.js
@@ -2,29 +2,30 @@
 /* global Resources */
 /* global Effects */
 
+// define some vars
 const MAX_LIVES = 3;
 const MAX_ENEMIES = 3;
 const MAX_COLLECTABLES = 3;
 const ENEMY_SPEED_MIN = 1;
 const ENEMY_SPEED_MAX = 4;
 const POINTS_WATER = 50;
-const POINTS_COLLECTABLE_STAR = 5;
-const POINTS_COLLECTABLE_KEY = 15;
 const POINTS_COLLECTABLE_GEM = 25;
-const POINTS_ENEMY_HIT = -5;
-const POINTS_LEVEL_2 = 400;
-const POINTS_LEVEL_3 = 800;
+const POINTS_COLLECTABLE_KEY = 15;
+const POINTS_COLLECTABLE_STAR = 5;
+const POINTS_ENEMY_HIT = -10;
+const LEVEL_2_TARGET_POINTS = 400;
+const LEVEL_3_TARGET_POINTS = 2 * LEVEL_2_TARGET_POINTS;
 const ROWS = 6;
 const COLS = 5;
-const ROW_OUTSIDE = -23;
-const COL_WIDTH = 101;
-const ROW_HEIGHT = 83;
-const topScores = [];
-let PLAYER_LEVEL = 1;
-let PLAYER_STRIPE;
-let GRID;
+const ROW_OUTSIDE = -23; // empty space at top on all images
+const COL_WIDTH = 101; // see engine.js:148
+const ROW_HEIGHT = 83; // see engine.js:148
+const topScores = []; // array of top scores
+let PLAYER_LEVEL = 1; // starting at level 1
+let PLAYER_STRIPE; // used to keep the selected stripe at the very beginning of the game on restart
+let GRID; // 2-dimensional array, used to place the collectables on the play ground
 
-// init player to prevent errors on console output on very first game load.
+// init player to prevent errors on console output on very first game load
 let player = {
   handleInput() {},
   update() {},
@@ -33,8 +34,11 @@ let player = {
 const allEnemies = [];
 const allCollectables = [];
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+/*
+ * @description Listens for key presses and sends the keys to Player.
+ * Possible Keys: Left, Up, Right, Down Arrow & h, j, k, l (VIM-style)
+ * @param {e} Event to listen for.
+ */
 const keyUpListener = e => {
   const allowedKeys = {
     37: 'left',
@@ -63,15 +67,15 @@ class Player {
   }
 
   /*
-  * @description Set x,y-coordinates of the player.
-  */
+   * @description Set x,y-coordinates of the player.
+   */
   reset() {
     this.col = Helper.getRandomInt(0, COLS - 1);
     this.row = ROWS - 1;
     this.x = Helper.getXCoordinate(this.col);
     this.y = Helper.getYCoordinate(this.row);
     Helper.resetGrid();
-    GRID[this.row][this.col] = true;
+    Helper.fillGridAt(this.row, this.col);
   }
 
   /*
@@ -95,23 +99,38 @@ class Player {
 
   /*
    * @description Update score of the player.
-   * @param {num} The number to update the score by.
+   * @param {num} The number to update the score by. Only integers allowed.
    */
   updateScoreBy(num) {
-    const tmpPoints = this.points + num;
-    this.points = tmpPoints < 0 ? 0 : tmpPoints;
-    document.querySelector('#points').textContent = this.points;
-    Helper.levelUp();
+    if (Number.isInteger(num)) {
+      const tmpPoints = this.points + num;
+      this.points = tmpPoints < 0 ? 0 : tmpPoints;
+      document.querySelector('#points').textContent = this.points;
+      Helper.levelUp();
+    }
   }
 
   /*
    * @description Update life of the player.
-   * @param {num} The number to update the life by.
+   * @param {num} The number to update the life by. Only integers allowed.
    */
   updateLive(num) {
-    const tmpLives = this.lives + num;
-    this.lives = tmpLives < 0 ? 0 : tmpLives;
-    this.drawLives();
+    if (Number.isInteger(num)) {
+      const tmpLives = this.lives + num;
+      this.lives = tmpLives < 0 ? 0 : tmpLives;
+      this.drawLives();
+    }
+  }
+
+  /*
+     * @description Update points, life of player and reset to initial state.
+     * @param {points} The points to be added.
+     * @param {lifeNum} The life to be added.
+     */
+  updateOnCollision(points, lifeNum) {
+    this.updateScoreBy(points);
+    this.updateLive(lifeNum);
+    this.reset();
   }
 
   /**
@@ -123,17 +142,11 @@ class Player {
       // show all
       Effects.show('#lives img', true);
     } else {
+      // player lost one live, so hide
       Effects.hide(`#lives img:nth-child(${this.lives + 1})`);
+      // game over?
       if (this.lives === 0) Helper.gameOver();
     }
-    // const heartImg = Resources.get('images/Heart.png');
-    // heartImg.alt = 'Heart';
-    // heartImg.width = 24;
-    // for (let i = 0; i < this.lives; i += 1) {
-    // outerHTML += heartImg.outerHTML;
-    // }
-    // }
-    // document.getElementById('lives').innerHTML = outerHTML;
   }
 
   /*
@@ -142,6 +155,9 @@ class Player {
    */
   handleInput(keyValue) {
     if (this.lives > 0) {
+      // remove old position on grid
+      Helper.clearGridAt(this.row, this.col);
+      // handle key input
       switch (keyValue) {
         case 'left':
           if (this.x >= COL_WIDTH) this.x -= COL_WIDTH;
@@ -157,35 +173,47 @@ class Player {
           break;
         default: // do nothing
       }
+      // set new position on grid
+      Helper.fillGridAt(this.row, this.col);
     }
   }
 }
 
-/** Collectable our player may collect to earn more points. */
+/** Collectable class. Player may collect to earn more points. */
 class Collectable {
   constructor(sprite = 'images/Star.png') {
     this.sprite = sprite;
-    // prettier-ignore
-    this.max_positions = (ROWS * COLS) - 1;
     this.reset();
   }
 
   /*
-   * @description Set x,y coordinates of the collectable.
+   * @description Set x,y coordinates of the collectable
+   * and find position on the grid.
    */
   reset() {
+    // prettier-ignore
+    this.max_positions_on_grid = (ROWS * COLS) - 1;
     this.col = Helper.getRandomInt(0, COLS - 1);
-    this.row = Helper.getRandomInt(0, ROWS - 1);
+    if (this.sprite.includes('Gem')) {
+      this.row = 0;
+    } else if (this.sprite.includes('Key')) {
+      this.row = Helper.getRandomInt(1, 3);
+    } else {
+      this.row = Helper.getRandomInt(0, ROWS - 1);
+    }
     this.findNext();
     this.x = Helper.getXCoordinate(this.col);
     this.y = Helper.getYCoordinate(this.row);
   }
 
   /*
-   * @description Find next empty space on grid
+   * @description Find next empty space on grid. Empty space means
+   * simply that on the desired position there is no collectable or player.
+   * If the position is occupied test the next position on the grid, until you find a place.
+   * Should all positions be occupied, don't do anything.
    */
   findNext() {
-    if (GRID[this.row][this.col]) {
+    if (Helper.getValueFromGridAt(this.row, this.col)) {
       // check for next free element
       if (this.row === GRID.length - 1) {
         this.row = 0;
@@ -195,12 +223,13 @@ class Collectable {
       } else {
         this.col += 1;
       }
-      if (this.max_positions > 0) {
+      // is there still empty place on the grid?
+      if (this.max_positions_on_grid > 0) {
         this.findNext();
       }
     } else {
-      GRID[this.row][this.col] = true;
-      this.max_positions -= 1;
+      Helper.fillGridAt(this.row, this.col);
+      this.max_positions_on_grid -= 1;
     }
   }
 
@@ -211,18 +240,22 @@ class Collectable {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   }
 
+  /*
+   * @description Check for collisions and update players score if necessary.
+   */
   checkCollisions() {
     if (this.x === player.x && this.y === player.y) {
+      // remove from grid
+      GRID[this.row][this.col] = false;
+      // place outside of game board
       this.x = 0 - COL_WIDTH;
       this.y = ROW_OUTSIDE;
 
+      // get points
       let pointsEarned = POINTS_COLLECTABLE_STAR;
-      if (this.sprite.includes('Key')) {
-        pointsEarned = POINTS_COLLECTABLE_KEY;
-      }
-      if (this.sprite.includes('Gem')) {
-        pointsEarned = POINTS_COLLECTABLE_GEM;
-      }
+      if (this.sprite.includes('Key')) pointsEarned = POINTS_COLLECTABLE_KEY;
+      if (this.sprite.includes('Gem')) pointsEarned = POINTS_COLLECTABLE_GEM;
+
       player.updateScoreBy(pointsEarned);
     }
   }
@@ -230,9 +263,8 @@ class Collectable {
 
 /** Enemies our player must avoid */
 class Enemy {
-  constructor() {
-    // The image/sprite for our enemies, this uses a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  constructor(sprite = 'images/enemy-bug.png') {
+    this.sprite = sprite;
     this.reset();
   }
 
@@ -248,21 +280,18 @@ class Enemy {
   }
 
   /*
-   * @description Update the enemy's position and check for collisions.
+   * @description Update the enemy's position.
    * @parameter {dt} a time delta between ticks.
    */
   update(dt) {
     // multiply any movement by the dt parameter which will ensure the game
     // runs at the same speed for all computers.
-    this.x += COL_WIDTH * dt * this.speed;
-    // enemy outside of canvas?
-    if (this.x > COLS * COL_WIDTH) {
-      this.x = -1 * COL_WIDTH;
-    }
-    if (this.checkCollisions()) {
-      player.reset();
-      player.updateScoreBy(POINTS_ENEMY_HIT);
-      player.updateLive(-1);
+    if (dt) {
+      this.x += COL_WIDTH * dt * this.speed;
+      // enemy outside of canvas?
+      if (this.x > COLS * COL_WIDTH) {
+        this.x = -1 * COL_WIDTH;
+      }
     }
   }
 
@@ -277,25 +306,40 @@ class Enemy {
    * @description Check for collision with the player and update score & life.
    */
   checkCollisions() {
-    const xSpace = this.x < player.x && this.x + ROW_HEIGHT > player.x;
-    const ySpace = this.y === player.y;
-    if (xSpace && ySpace) {
-      player.reset();
-      player.updateScoreBy(POINTS_ENEMY_HIT);
-      player.updateLive(-1);
+    // is the player in the range of the enemy?
+    const xPosTolerance = COL_WIDTH / 2;
+    const xPos = this.x >= player.x - xPosTolerance && this.x < player.x + xPosTolerance;
+    const yPos = this.y === player.y;
+
+    if (xPos && yPos) {
+      // reset player, update score & remove one live.
+      player.updateOnCollision(POINTS_ENEMY_HIT, -1);
     }
   }
 
+  /*
+   * @description Sets speed to zero and stops movement.
+   */
   stopMoving() {
     this.speed = 0;
   }
 
-  speedUpBy(num) {
-    this.speed += num;
+  /*
+   * @description Speed up enemy by the given number.
+   * @param {num} The number to add to the speed. Only positive integers allowed.
+   */
+  accelerateBy(num) {
+    if (Number.isInteger(num) && num > 0) {
+      this.speed += num;
+    }
   }
 }
 
+/** Helper class to have common actions in one place */
 class Helper {
+  /*
+   * @description Initial game and set player, collectables and enemies initial values.
+   */
   static init(event) {
     return new Promise((resolve, reject) => {
       // clear all
@@ -306,14 +350,17 @@ class Helper {
         PLAYER_STRIPE =
           event && event.target.attributes ? event.target.attributes.src.value : undefined;
       }
-      // create game objects
+      // create player
       player = new Player(PLAYER_STRIPE);
+      // create enemies
       for (let i = 0; i < MAX_ENEMIES; i += 1) {
         allEnemies.push(new Enemy());
       }
+      // create collectables
       for (let i = 0; i < MAX_COLLECTABLES; i += 1) {
         allCollectables.push(new Collectable());
       }
+      // resolve if everything is set up else reject
       if (player && allEnemies.length > 0 && allCollectables.length > 0) {
         resolve();
       } else {
@@ -322,12 +369,15 @@ class Helper {
     });
   }
 
+  /*
+   * @description Calculate speed of enemy
+   */
   static calculateEnemySpeed() {
     // prettier-ignore
     return (Math.random() * (ENEMY_SPEED_MAX - ENEMY_SPEED_MIN)) + ENEMY_SPEED_MIN;
   }
 
-  /**
+  /*
    * @description Calculate the X coordinate.
    * @param {x} The value of the X coordinate to calculate.
    * @return {number} A number representing X coordinate multiplied by COL_WIDTH.
@@ -336,7 +386,7 @@ class Helper {
     return x * COL_WIDTH;
   }
 
-  /**
+  /*
    * @description Calculate the Y coordinate.
    * @param {y} The value of the Y coordinate to calculate.
    * @return {number} A number representing Y coordinate multiplied by ROW_HEIGHT + ROW_OUTSIDE.
@@ -346,21 +396,27 @@ class Helper {
     return (y * ROW_HEIGHT) + ROW_OUTSIDE;
   }
 
+  /*
+   * @description Will be invoked on game over to display 'Top Scores' to the user.
+   */
   static gameOver() {
-    this.reOrderTopScores();
+    this.descOrderTopScores();
     // display stats
     Effects.remove('canvas')
       .then(Effects.show('#topScores'))
       .then(Effects.show('#restartGame'))
       .then(() => {
-        // stop enemies from moving
+        // stop enemies from moving to prevent continious calculation of movement.
         allEnemies.forEach(enemy => enemy.stopMoving());
-        Helper.displayStats();
+        Helper.displayTopScores();
       })
-      .catch(e => console.error(e));
+      .catch(e => console.error(e)); // eslint-disable-line no-console
   }
 
-  static displayStats() {
+  /*
+   * @description Create 'Top Scores' statistic.
+   */
+  static displayTopScores() {
     const topPoints = document.getElementById('topPoints');
     topPoints.innerHTML = '';
     topScores.forEach((elem, idx) => {
@@ -370,7 +426,10 @@ class Helper {
     });
   }
 
-  static reOrderTopScores() {
+  /*
+   * @description Sort Top Scores in descending order.
+   */
+  static descOrderTopScores() {
     if (!topScores.includes(player.points)) {
       topScores.push(player.points);
       // sort items by descending order
@@ -381,55 +440,113 @@ class Helper {
     }
   }
 
+  /*
+   * @descripton Restart game which will invoke Helper.init()
+   * method to initial a new game.
+   */
   static restartGame() {
     Effects.remove('#topScores')
       .then(Effects.remove('#restartGame'))
       .then(Effects.show('canvas'))
       .then(this.init())
-      .catch(e => console.error(e));
+      .catch(e => console.error(e)); // eslint-disable-line no-console
   }
 
+  /*
+   * @description Reset grid, which is used to place collectables on the game board intelligently.
+   */
   static resetGrid() {
     GRID = Array(ROWS)
       .fill()
       .map(() => Array(COLS).fill(false));
   }
 
+  /*
+   * @description Set the value at the given position to true, which
+   * indicates an occupied position on the game board.
+   * @param {y} The y coordinate.
+   * @param {x} The x coordinate.
+   * @return {boolean} Return true on success else false.
+   */
+  static fillGridAt(y, x) {
+    if (GRID && GRID.length > y && GRID[y].length > x) {
+      GRID[y][x] = true;
+      return true;
+    }
+    return false;
+  }
+
+  /*
+   * @description Set the value at the given position to false, which
+   * indicates an empty space on the game board.
+   * @param {y} The y coordinate.
+   * @param {x} The x coordinate.
+   * @return {boolean} Return true on success else false.
+   */
+  static clearGridAt(y, x) {
+    if (GRID && GRID.length > y && GRID[y].length > x) {
+      GRID[y][x] = false;
+      return true;
+    }
+    return false;
+  }
+
+  /*
+   * @description Get value of the specified position.
+   * @param {y} The y coordinate.
+   * @param {x} The x coordinate.
+   * @return {boolean} Returns true, if only the value is true, else false.
+   */
+  static getValueFromGridAt(y, x) {
+    return GRID && GRID.length > y && GRID[y].length > x ? GRID[y][x] : false;
+  }
+
+  /*
+   * @description Increase game level, if the user reaches specific points.
+   * If the player reaches Level 2 || Level 3, an enemy and collectable will
+   * be added extra on the game board accordingly and the enemies will get faster.
+   */
   static levelUp() {
-    if (player.points < POINTS_LEVEL_2) {
+    if (player.points < LEVEL_2_TARGET_POINTS) {
       PLAYER_LEVEL = 1;
     }
-    if (PLAYER_LEVEL === 1 && player.points >= POINTS_LEVEL_2) {
+    if (PLAYER_LEVEL === 1 && player.points >= LEVEL_2_TARGET_POINTS) {
       allEnemies.push(new Enemy());
       allCollectables.push(new Collectable('images/Key.png'));
-      allEnemies.forEach(enemy => enemy.speedUpBy(1));
+      allEnemies.forEach(enemy => enemy.accelerateBy(1));
       PLAYER_LEVEL = 2;
     }
-    if (PLAYER_LEVEL === 2 && player.points >= POINTS_LEVEL_3) {
+    if (PLAYER_LEVEL === 2 && player.points >= LEVEL_3_TARGET_POINTS) {
       allEnemies.push(new Enemy());
       allCollectables.push(new Collectable('images/Gem Blue.png'));
-      allEnemies.forEach(enemy => enemy.speedUpBy(2));
+      allEnemies.forEach(enemy => enemy.accelerateBy(2));
       PLAYER_LEVEL = 3;
     }
     document.querySelector('#level').textContent = PLAYER_LEVEL;
   }
 
+  /*
+   * @description Clear an array.
+   */
   static clearArray(arr) {
     while (arr && arr.length) {
       arr.pop();
     }
   }
 
-  /**
+  /*
    * @description Return randomly generated number including min/max value.
    * @param {min} The minimum value.
    * @param {max} The maximum value.
    * @return {number} A number representing the given range. Returns 0, if the range is not valid.
    */
   static getRandomInt(min, max) {
-    const tmpMin = Math.ceil(min);
-    const tmpMax = Math.floor(max);
-    // prettier-ignore
-    return Math.floor(Math.random() * ((tmpMax - tmpMin) + 1)) + tmpMin;
+    if (Number.isInteger(min) && Number.isInteger(max)) {
+      const tmpMin = Math.ceil(min);
+      const tmpMax = Math.floor(max);
+      // prettier-ignore
+      return Math.floor(Math.random() * ((tmpMax - tmpMin) + 1)) + tmpMin;
+    }
+    return 0;
   }
 }
